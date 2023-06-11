ad13594ad037a0cb24781195c0f24c81ed625773
xuanyu.li <xuanyu.li@baishan.com>
2023-06-11 14:58:16 +0800

    feature: demo整合zinx配置

---
+++ cmd/demo/internal/dao/gen_model/user.go
@@ -0,0 +1,11 @@
+package gen_model
+
+import "github.com/go-nova/cmd/demo/internal/dao/model"
+
+type UserMutationObject struct {
+	Register []*model.User `json:"register"`
+}
+
+type UserQueryObject struct {
+	UserList []*model.User `json:"userList"`
+}
