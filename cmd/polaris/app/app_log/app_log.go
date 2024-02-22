fdac07e3554b8c308ff09bf9d7b03b833f364668
xuanyu <2872326162@qq.com>
2024-02-22 11:19:51 +0800

    feature: 基于graph-gophers的graphql示例上传

--- cmd/polaris/app/app_log/app_log.go
+++ cmd/polaris/app/app_log/app_log.go
@@ -1,13 +1,12 @@
 package app_log
 
 import (
-	myzap "github.com/go-nova/pkg/contrib/log/zap/v2"
 	"github.com/go-nova/pkg/core/log"
 )
 
 func InitLogInstance() *log.Logger {
-	logger := myzap.NewLoggerInstance("level", "note", "app_log")
-	defer func() { _ = logger.Close() }()
+	//logger := myzap.NewLoggerInstance("level", "note", "app_log")
+	//defer func() { _ = logger.Close() }()
 
-	return logger
+	return nil
 }
